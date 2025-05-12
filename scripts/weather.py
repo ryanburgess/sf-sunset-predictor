2d279cd0b7514a7bcb74b51fe0c5685c1dd8287b
Ryan Burgess <burgessdryan@gmail.com>
2025-05-12 16:16:41 -0700

    fix weather

--- scripts/weather.py
+++ scripts/weather.py
@@ -3,7 +3,7 @@ import json
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 API_KEY = os.getenv("WEATHERAPI_KEY")
 LOCATION = "San Francisco"
